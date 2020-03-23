8a88d59cf97a07f6fe565107f6d8cad23cdcccca
elisee <elisee.kalonji99@gmail.com>
2020-03-23 07:14:03 -0700

    changing the copyright

--- layout/footer.php
+++ layout/footer.php
@@ -143,7 +143,7 @@
                             Copyright &copy;
                             <script>
                                 document.write(new Date().getFullYear());
-                            </script> Tout droit réservé | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                            </script> Tout droit réservé | Designed by DTeam
                             <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
                         </p>
                     </div>
